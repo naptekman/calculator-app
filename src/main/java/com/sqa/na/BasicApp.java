c659d377078531028566261c23d35772b93e7e9e
Nina Aptekman <nina.aptekman@gmail.com>
2017-08-12 16:10:02 -0700

    Create project structure

---
+++ src/main/java/com/sqa/na/BasicApp.java
@@ -0,0 +1,29 @@
+/**
+ * File Name: BasicApp.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Aug 12, 2017
+ */
+package com.sqa.na;
+
+/**
+ * BasicApp //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class BasicApp {
+
+	public static void main(String[] args) {
+		System.out.print("┏(-_-)┛");
+		System.out.print("┏(-_-)┛");
+		System.out.print("┏(-_-)┛");
+		System.out.print("┏(-_-)┛\n");
+	}
+}
